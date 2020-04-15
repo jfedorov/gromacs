a735d3c236814e75fa66ad09228e8ddfb7a06869
M. Eric Irrgang <ericirrgang@gmail.com>
2020-04-15 19:52:30 +0300

    Set OpenMP threads for gmxapi test executables.
    Resolve an issue in which the GmxapiMpiTests GTest could cause a fatal
    error when a test environment has more detected virtual cores than the
    GROMACS library thinks it should try to use.
    
    Fixes #3485

--- src/api/cpp/tests/testingconfiguration.h
+++ src/api/cpp/tests/testingconfiguration.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2018,2019, by the GROMACS development team, led by
+ * Copyright (c) 2018,2019,2020, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -115,6 +115,8 @@ public:
     {
         std::vector<std::string> mdArgs;
 
+        mdArgs.emplace_back("-ntomp");
+        mdArgs.emplace_back("1");
         mdArgs.emplace_back("-o");
         mdArgs.emplace_back(runner_.fullPrecisionTrajectoryFileName_);
         mdArgs.emplace_back("-x");
