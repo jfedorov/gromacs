ed3bb301e7193f4ce9b1dabc87493c6e1074ea08
Berk Hess <hess@kth.se>
2021-02-22 14:05:20 +0100

    Clean up index handing in make_bondeds_zone
    Alo changed zones arguments to const references.

--- src/gromacs/domdec/partition.cpp
+++ src/gromacs/domdec/partition.cpp
@@ -3192,7 +3192,7 @@ void dd_partition_system(FILE*                     fplog,
         np[dd->dim[i]] = comm->cd[i].numPulses();
     }
     dd_make_local_top(dd,
-                      &comm->zones,
+                      comm->zones,
                       dd->unitCellInfo.npbcdim,
                       state_local->box,
                       comm->cellsize_min,
