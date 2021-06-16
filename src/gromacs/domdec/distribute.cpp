b98e45f0006523e197c6f160fcc3eecf171dd0f0
Berk Hess <hess@kth.se>
2021-06-16 16:01:53 +0200

    Work around static-analyzer warning

--- src/gromacs/domdec/distribute.cpp
+++ src/gromacs/domdec/distribute.cpp
@@ -271,8 +271,11 @@ static void dd_distribute_state(gmx_domdec_t* dd, const t_state* state, t_state*
     {
         distributeVec(dd, DDMASTER(dd) ? state->v : gmx::ArrayRef<const gmx::RVec>(), state_local->v);
     }
-    GMX_RELEASE_ASSERT(!DDMASTER(dd) || state->rvecVectors().size() == state_local->rvecVectors().size(),
-                       "The number of rvecVectors in the global and local state should match");
+    if (DDMASTER(dd))
+    {
+        GMX_RELEASE_ASSERT(state->rvecVectors().size() == state_local->rvecVectors().size(),
+                           "The number of rvecVectors in the global and local state should match");
+    }
     // On non-master ranks we don't use globalRVecVectorsIt and we assign the local vector iterator
     // begin value to avoid type issues
     auto globalRVecVectorsIt =
