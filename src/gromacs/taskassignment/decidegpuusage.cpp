6b24c0a292e0b34fafc90ad85de981b8c4c2b3ed
Andrey Alekseenko <al42and@gmail.com>
2021-09-19 19:23:41 +0300

    Enable SYCL update with constraints

--- src/gromacs/taskassignment/decidegpuusage.cpp
+++ src/gromacs/taskassignment/decidegpuusage.cpp
@@ -607,13 +607,9 @@ bool decideWhetherToUseGpuForUpdate(const bool                     isDomainDecom
     {
         errorMessage += "Compatible GPUs must have been found.\n";
     }
-    if (!GMX_GPU_CUDA && !GMX_GPU_SYCL)
+    if (!(GMX_GPU_CUDA || GMX_GPU_SYCL))
     {
         errorMessage += "Only CUDA and SYCL builds are supported.\n";
-    }
-    if (GMX_GPU_SYCL && hasAnyConstraints)
-    {
-        errorMessage += "Constraints are not supported in SYCL build.\n";
     }
     if (inputrec.eI != IntegrationAlgorithm::MD)
     {
