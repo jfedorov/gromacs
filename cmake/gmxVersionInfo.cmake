cbffe9813f0db3fc89e420a80b88b7e67db5164b
Mark Abraham <mark.j.abraham@gmail.com>
2021-10-07 16:07:22 +0200

    Prepare for first 2022 beta release

--- cmake/gmxVersionInfo.cmake
+++ cmake/gmxVersionInfo.cmake
@@ -204,7 +204,7 @@ set(GMX_VERSION_PATCH 0)
 # candidates, where it signifies the most recent such release from
 # this branch; it will be empty before the first such release, as well
 # as after the final release is out.
-set(GMX_VERSION_SUFFIX "")
+set(GMX_VERSION_SUFFIX "-beta1")
 
 # Conventionally with libtool, any ABI change must change the major
 # version number, the minor version number should change if it's just
@@ -231,12 +231,14 @@ set(GMX_VERSION_STRING "${GMX_VERSION}${GMX_VERSION_SUFFIX}")
 
 set(REGRESSIONTEST_VERSION "${GMX_VERSION_STRING}")
 set(REGRESSIONTEST_BRANCH "master")
-# Run the regressiontests packaging job with the correct pakage
-# version string, and the release box checked, in order to have it
-# build the regressiontests tarball with all the right naming. The
-# naming affects the md5sum that has to go here, and if it isn't right
+# Follow the release checklist (at
+# https://gitlab.com/gromacs/gromacs/-/wikis/Release-checklist) and in
+# particular the instructions under "How to build a regressiontests
+# tarball" in order to have it build the regressiontests tarball with
+# all the right version naming. The version number and suffix affects
+# the md5sum that has to go here, and if it isn't right the real
 # release workflow will report a failure.
-set(REGRESSIONTEST_MD5SUM "93956ea42c4d16fdd541518c05972989" CACHE INTERNAL "MD5 sum of the regressiontests tarball for this GROMACS version")
+set(REGRESSIONTEST_MD5SUM "TODO" CACHE INTERNAL "MD5 sum of the regressiontests tarball for this GROMACS version")
 
 # If you are making a custom fork of GROMACS, please describe your
 # fork, perhaps with its version number, in the value of
