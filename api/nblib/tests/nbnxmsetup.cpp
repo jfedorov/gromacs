057060db231559bb62dc557a7a58b902524471a6
ejjordan <ejjordan@kth.se>
2021-10-08 17:04:38 +0200

    fix errors

--- api/nblib/tests/nbnxmsetup.cpp
+++ api/nblib/tests/nbnxmsetup.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of the GROMACS molecular simulation package.
  *
- * Copyright (c) 2020,2021, by the GROMACS development team, led by
+ * Copyright (c) 2021, by the GROMACS development team, led by
  * Mark Abraham, David van der Spoel, Berk Hess, and Erik Lindahl,
  * and including many others, as listed in the AUTHORS file in the
  * top-level source directory and at http://www.gromacs.org.
@@ -57,6 +57,10 @@
 #include "testutils/test_hardware_environment.h"
 
 namespace nblib
+{
+namespace test
+{
+namespace
 {
 
 TEST(NbnxmSetupTest, findNumEnergyGroups)
@@ -251,4 +255,6 @@ TEST(NbnxmSetupTest, CreateNbnxmGPUThrowsInvalidKernelChoice)
 }
 #endif
 
+} // namespace
+} // namespace test
 } // namespace nblib
