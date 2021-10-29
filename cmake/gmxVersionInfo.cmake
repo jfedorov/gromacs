jfedorov/gromacs

commit dc25e5beb2f8c0c9eea3e00130cd1882570e695a

Use CMake project versioning convention.
* Set CMP0048 policy to NEW behavior.
* Set MAJOR and MINOR version in the main project() command. Update
  `cmake/gmxVersionInfo.cmake` to set from PROJECT_VERSION_MAJOR and
  PROJECT_VERSION_MINOR.

Fixes #3725

cmake/gmxVersionInfo.cmake
@@ -36,11 +36,14 @@
 # Sets version information variables and provides CMake functions for
 # generating files based on them
 #
-# This script provides the following basic version variables that need to be
-# maintained manually:
+# The following variables are derived from variables initialized by
+# https://cmake.org/cmake/help/latest/command/project.html#command:project
 #   GMX_VERSION_MAJOR      Major version number.
 #   GMX_VERSION_PATCH      Patch version number.
 #       Should always be defined: zero for, e.g., 2016.
+#
+# This script provides the following basic version variables that need to be
+# maintained manually:
 #   GMX_VERSION_SUFFIX     String suffix to add to numeric version string.
 #       "-dev" is automatically added when not building from a source package,
 #       and does not need to be kept here. This mechanism is not quite enough
@@ -194,13 +197,15 @@
 # are used internally by this machinery, as well as VersionInfo.cmake.cmakein.
 
 #####################################################################
+# Derived version info.
+# Ref https://cmake.org/cmake/help/latest/command/project.html#command:project
+set(GMX_VERSION_MAJOR ${Gromacs_VERSION_MAJOR})
+set(GMX_VERSION_PATCH ${Gromacs_VERSION_MINOR})
+
+#####################################################################
 # Manually maintained version info
 
-# The GROMACS convention is that these are the version number of the next
-# release that is going to be made from this branch.
-set(GMX_VERSION_MAJOR 2022)
-set(GMX_VERSION_PATCH 0)
-# The suffix, on the other hand, is used mainly for betas and release
+# The suffix is used mainly for betas and release
 # candidates, where it signifies the most recent such release from
 # this branch; it will be empty before the first such release, as well
 # as after the final release is out.

CMakeLists.txt
@@ -37,6 +37,7 @@
 cmake_minimum_required(VERSION 3.16.3)
 cmake_policy(SET CMP0074 NEW) # From CMake 3.12
 cmake_policy(SET CMP0068 NEW) # From CMake-3.9
+cmake_policy(SET CMP0048 NEW) # As of CMake 3.22, default is still "OLD"
 
 # CMake modules/macros are in a subdirectory to keep this file cleaner
 # This needs to be set before project() in order to pick up toolchain files
@@ -53,7 +54,9 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
         FORCE)
 endif()
 
-project(Gromacs)
+# The GROMACS convention is that these are the version number of the next
+# release that is going to be made from this branch.
+project(Gromacs VERSION 2022.0)
 
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)

commit 4f4a79ba42a1f268a36241f2f6a56a5497e13db6

Use CMake to propagate versions and hashes to gitlab jobs
Various CI jobs need version and md5sum information. These are now
propagated via a file that was anyway always written by CMake at
configure time. This fixes things inadvertently broken in dc25e5be.

The fix removes a job from the dependency stack of the release
workflow, since the necessary operation is now always done as a side
effect of every configuration.

Refs #3275

admin/gitlab-ci/archive.gitlab-ci.yml
@@ -1,22 +1,5 @@
 # Packages, exported artifacts, and release engineering processes.
 
-prepare-release-version:
-  extends:
-    - .variables:default
-    - .rules:nightly-only-for-release
-  cache: {}
-  image: ${CI_REGISTRY}/gromacs/gromacs/ci-ubuntu-20.04-llvm-7-docs
-  stage: configure-build
-  variables:
-    KUBERNETES_CPU_LIMIT: 1
-    KUBERNETES_CPU_REQUEST: 1
-    KUBERNETES_MEMORY_REQUEST: 2Gi
-  script:
-    - cmake -P cmake/gmxVersionInfo.cmake &> version.json
-  artifacts:
-    paths:
-      - version.json
-
 # Special job to package regressiontest files and have them available for testing
 # Runs during pre-build
 # Set up to only fetch the files and prepare everything for merge requests
@@ -75,11 +58,8 @@ regressiontests:package:
     KUBERNETES_CPU_LIMIT: 1
     KUBERNETES_CPU_REQUEST: 1
     KUBERNETES_MEMORY_REQUEST: 2Gi
-  needs:
-    - job: prepare-release-version
   script:
-    - VERSION=`cat version.json |
-      python3 -c "import json,sys; print(json.load(sys.stdin)['version'])"`
+    - VERSION=`sed -n -e '/GMX_VERSION_STRING\b/ s/.*"\(.*\)")/\1/p' VersionInfo-partial.cmake.cmakein`
     - REGTESTNAME="regressiontests-"$VERSION
     - if [[ $GROMACS_RELEASE != "true" ]] ; then
       REGTESTNAME=$REGTESTNAME-dev ;
@@ -222,13 +202,11 @@ checksum-verify:
     BUILD_DIR: build-package
   script:
     - VALID_BUILD=true
-    - VERSION=`cat version.json |
-      python3 -c "import json,sys; print(json.load(sys.stdin)['version'])"`
+    - VERSION=`sed -n -e '/GMX_VERSION_STRING\b/ s/.*"\(.*\)")/\1/p' VersionInfo-partial.cmake.cmakein`
     - if [[ "$GROMACS_RELEASE" != "true" ]] ; then
       VERSION="$VERSION"-dev ;
       fi
-    - REGTEST_COMPARE=`cat version.json |
-      python3 -c "import json,sys; print(json.load(sys.stdin)['regressiontest-md5sum'])"`
+    - REGTEST_COMPARE=`sed -n -e '/REGRESSIONTEST_MD5SUM/ s/.*"\(.*\)")/\1/p' VersionInfo-partial.cmake.cmakein`
     - SOURCENAME=gromacs-"$VERSION"
     - SOURCETARBALL="$SOURCENAME".tar.gz
     - SOURCE_MD5SUM=`md5sum "$SOURCETARBALL" | awk '{print $1}'`
@@ -248,7 +226,6 @@ checksum-verify:
   dependencies:
     - archive:package
     - regressiontests:package
-    - prepare-release-version
 
 version-verify:
   image: ${CI_REGISTRY}/gromacs/gromacs/ci-ubuntu-20.04-llvm-7-docs
@@ -265,8 +242,7 @@ version-verify:
     BUILD_DIR: release-doc-builds
   script:
     - VALID_BUILD=true
-    - VERSION=`cat version.json |
-      python3 -c "import json,sys; print(json.load(sys.stdin)['version'])"`
+    - VERSION=`sed -n -e '/GMX_VERSION_STRING\b/ s/.*"\(.*\)")/\1/p' VersionInfo-partial.cmake.cmakein`
     - if [[ "$GROMACS_RELEASE" != "true" ]] ; then
       VERSION="$VERSION"-dev ;
       fi
@@ -282,7 +258,6 @@ version-verify:
       fi
   dependencies:
     - webpage:dependencies
-    - prepare-release-version
 
 archive:nightly-webpage:
   extends:

admin/gitlab-ci/documentation.gitlab-ci.yml
@@ -109,13 +109,11 @@
     - echo $CMAKE_BUILD_TYPE_OPTIONS
     - echo $CMAKE_GMXAPI_OPTIONS
     - echo $REL_OPTION
-    - VERSION=`cat version.json |
-      python3 -c "import json,sys; print(json.load(sys.stdin)['version'])"`
+    - VERSION=`sed -n -e '/GMX_VERSION_STRING\b/ s/.*"\(.*\)")/\1/p' VersionInfo-partial.cmake.cmakein`
     - if [[ $GROMACS_RELEASE != "true" ]] ; then
       VERSION=$VERSION-dev ;
       fi
-    - REGTEST_COMPARE=`cat version.json |
-      python3 -c "import json,sys; print(json.load(sys.stdin)['regressiontest-md5sum'])"`
+    - REGTEST_COMPARE=`sed -n -e '/REGRESSIONTEST_MD5SUM/ s/.*"\(.*\)")/\1/p' VersionInfo-partial.cmake.cmakein`
     - SOURCENAME=gromacs-$VERSION
     - SOURCETARBALL=$SOURCENAME.tar.gz
     - REGTESTNAME=regressiontests-$VERSION
@@ -282,7 +280,6 @@ webpage:configure:
   dependencies:
     - archive:package
     - regressiontests:package
-    - prepare-release-version
   variables:
     CMAKE_EXTRA_OPTIONS: -DGMX_BUILD_HELP=on -DGMX_BUILD_MANUAL=on -DGMX_USE_SIMD_KERNELS=off
 

admin/gitlab-ci/gromacs.matrix.gitlab-ci.yml
@@ -47,8 +47,7 @@
     RELEASE_REGRESSIONTESTS: release-regressiontests-from-tarball
   script:
     - CMAKE=${CMAKE:-$(which cmake)}
-    - VERSION=`cat version.json |
-      python3 -c "import json,sys; print(json.load(sys.stdin)['version'])"`
+    - VERSION=`sed -n -e '/GMX_VERSION_STRING\b/ s/.*"\(.*\)")/\1/p' VersionInfo-partial.cmake.cmakein`
     - if [[ $GROMACS_RELEASE != "true" ]] ; then
       VERSION=$VERSION-dev ;
       fi

cmake/VersionInfo.cmake.cmakein
@@ -42,3 +42,5 @@ set(GMX_VERSION_FULL_HASH         "@GMX_VERSION_FULL_HASH@")
 set(GMX_VERSION_CENTRAL_BASE_HASH "@GMX_VERSION_CENTRAL_BASE_HASH@")
 set(GMX_RELEASE_SOURCE_FILE_CHECKSUM  "@GMX_RELEASE_SOURCE_FILE_CHECKSUM@")
 set(GMX_CURRENT_SOURCE_FILE_CHECKSUM  "@GMX_CURRENT_SOURCE_FILE_CHECKSUM@")
+
+set(REGRESSIONTEST_MD5SUM             "@REGRESSIONTEST_MD5SUM@")

cmake/gmxVersionInfo.cmake
@@ -267,21 +267,13 @@ option(GMX_BUILD_TARBALL "Build tarball without -dev version suffix" OFF)
 mark_as_advanced(GMX_BUILD_TARBALL)
 # If run with cmake -P, the -dev suffix is managed elsewhere.
 if (NOT SOURCE_IS_SOURCE_DISTRIBUTION AND
-    NOT GMX_BUILD_TARBALL AND
-    NOT CMAKE_SCRIPT_MODE_FILE)
+    NOT GMX_BUILD_TARBALL)
     set(GMX_VERSION_STRING "${GMX_VERSION_STRING}-dev")
 endif()
 
 math(EXPR GMX_VERSION_NUMERIC
      "${GMX_VERSION_MAJOR}*10000 + ${GMX_VERSION_PATCH}")
 set(GMX_API_VERSION ${GMX_VERSION_NUMERIC})
-
-# If run with cmake -P from GitLab scripts, print out necessary version info
-# as JSON.
-if (CMAKE_SCRIPT_MODE_FILE)
-    message("{ \"version\": \"${GMX_VERSION_STRING}\", \"regressiontest-md5sum\": \"${REGRESSIONTEST_MD5SUM}\" }")
-    return()
-endif()
 
 # Set those values only in release versions, after getting the identifiers
 # from Zenodo for the manual and source code